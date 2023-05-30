0fb663bf2c7b45fc8d1a64f143b5c406d74bfada
HaydenP96 <HNPN1996@proton.me>
2023-05-30 13:52:21 -0700

    initial homepage creation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.render('index', { auth: 'true' });
+  res.render('index', { auth: 'false' });
 });
 
 app.listen(3000);
